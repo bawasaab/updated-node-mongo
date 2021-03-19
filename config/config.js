4a6b8b5e2ff431693e9a21967bf0e15a7413c209
Deepak Bawa <bawa_d@ymail.com>
2021-03-19 22:39:09 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
-const mongodbUrl = 'mongodb+srv://d190129k_chikitsa:z@UKqEpMU@cluster0.3xkqp.mongodb.net/chikitsa';
-// const mongodbUrlBck = 'mongodb+srv://d190129k_chikitsa:z%40UKqEpMU@cluster0.3xkqp.mongodb.net/test?authSource=admin&replicaSet=atlas-17zrkx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true';
+const mongodbUrl = "test";
 let basePath = 'https://mongodb-app-2.herokuapp.com';
 module.exports = {
     mongodbUrl: mongodbUrl,
